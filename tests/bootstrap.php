313c70cb17c5cdc70b37ccd389670bc47eeb48e3
Wojciech Peisert <wojtekp@sauron.pl>
2018-09-10 13:18:26 +0200

    add bootstrap

---
+++ tests/bootstrap.php
@@ -0,0 +1,27 @@
+<?php
+/*
+if (!defined('PIMCORE_PROJECT_ROOT')) {
+    define(
+        'PIMCORE_PROJECT_ROOT',
+        getenv('PIMCORE_PROJECT_ROOT')
+            ?: getenv('REDIRECT_PIMCORE_PROJECT_ROOT')
+            ?: realpath(getcwd())
+    );
+}
+
+if (!defined('TESTS_PATH')) {
+    define('TESTS_PATH', __DIR__);
+}
+
+define('PIMCORE_TEST', true);
+
+if (file_exists(PIMCORE_PROJECT_ROOT.'/pimcore/config/bootstrap.php')) {
+    require_once PIMCORE_PROJECT_ROOT.'/pimcore/config/bootstrap.php';
+}
+else {
+    \Pimcore\Bootstrap::setProjectRoot();
+    \Pimcore\Bootstrap::boostrap();
+}
+
+
+*/
